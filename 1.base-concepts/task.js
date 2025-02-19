adf03e8991f959b5daf312ddf96a0cb766baa813
unknown <181259602+GLancer14@users.noreply.github.com>
2025-02-19 11:39:22 +0400

    bjs-hw-1. All tasks solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  let discriminant = b ** 2 - (4 * a * c);
+  const arr = [];
+  const discriminant = b ** 2 - (4 * a * c);
   if (discriminant === 0) {
     arr.push(-b / (2 * a));
   } else if (discriminant > 0) {
     arr.push((-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a));
   }
-  
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  const monthPercent = percent / 100 / 12;
+  const monthPercentDecimalFraction = percent / 100 / 12;
   const creditBody = amount - contribution;
-  const monthPayment = creditBody * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
+  const monthPayment = creditBody * (monthPercentDecimalFraction + (monthPercentDecimalFraction / (((1 + monthPercentDecimalFraction) ** countMonths) - 1)));
   return Number((monthPayment * countMonths).toFixed(2));
 }
